12a15a767ae6ee9e4d7dcbbb9d14d972744732a2
郭宇 <guoyu@daling.com>
2016-08-16 09:20:25 +0800

    Make SpringHttpVerticle.start async

--- src/main/java/io/vertx/ext/spring/impl/SpringHttpVerticle.java
+++ src/main/java/io/vertx/ext/spring/impl/SpringHttpVerticle.java
@@ -3,6 +3,7 @@ package io.vertx.ext.spring.impl;
 import org.springframework.context.ApplicationContext;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.ext.spring.ContextFactory;
 import io.vertx.ext.web.Router;
@@ -24,13 +25,26 @@ public class SpringHttpVerticle extends AbstractVerticle {
   }
 
   @Override
-  public void start() throws Exception {
-    super.start();
-    VertxHolder.set(getVertx());
-    applicationContext = factory.create();
-    Router router = applicationContext.getBean(Router.class);
-    vertx.createHttpServer(options)
-      .requestHandler(router::accept)
-      .listen();
+  public void start(Future<Void> startFuture) throws Exception {
+    vertx.executeBlocking((f) -> {
+      try {
+        VertxHolder.set(getVertx());
+        applicationContext = factory.create();
+        f.complete(applicationContext);
+      } catch (Exception e) {
+        f.fail(e);
+      }
+    }, (ar) -> {
+      if (ar.failed()) {
+        startFuture.fail(ar.cause());
+      } else {
+        Router router = applicationContext.getBean(Router.class);
+        vertx.createHttpServer(options)
+          .requestHandler(router::accept)
+          .listen();
+        startFuture.complete(null);
+      }
+    });
   }
+
 }
